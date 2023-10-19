ef78b70827ffdd8fb20b42d741f049c7f0047133
yougret1 <1935109257@qq.com>
2023-10-19 16:48:04 +0800

    feat : 二重目录静态设置完成，除了focus

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,13 @@
 <style lang="scss">
 @import "./styles/index.scss";
 @import "./styles/toFormat.scss";
+
 .icon {
   width: 40px;
   height: 40px;
 }
-#app{
+
+#app {
   height: 100%;
   width: 100%;
   display: flex;
@@ -21,6 +23,16 @@
   align-items: center;
   background-color: #F2F2F2;
   background-image: url(./assets/images/texture.png);
+}
 
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: left;
+  background-color: #fff;
 }
 </style>
--- src/views/secCatalog/catalogExamSection/CatalogExamSection.vue
+++ src/views/secCatalog/catalogExamSection/CatalogExamSection.vue
@@ -1,38 +1,24 @@
 <!--  -->
 <template>
-  <div style="display: flex;flex-direction: column;align-items: center;">
-    <hr class=" m-5 mt-6 mb-6 bv_go_line">
-    <section class="mt-5 ">
-      <h2 class="mb-3">2022年</h2>
-      <div class="row justify-content-left year-row">
-        <div class="col-12"><a class="link-primary" href="/kaoyan/2022/01">
-            <div class="card">
-              <picture>
-                <source srcset=""
-                  type="image/webp">
-                <source srcset=""
-                  type="image/jpg"><img class="card-img-top"
-                  src=""
-                  style="width:280px;height:395px;" alt="2022年考研英语一">
-              </picture>
-              <div class="card-body">
-                <div class="title_num">2022年考研英语一</div><progress max="100" value="97"></progress>
-              </div>
-            </div>
-          </a><a class="link-primary" href="/kaoyan/2022/02">
-            <div class="card"><img class="card-img-top"
-                data-src=""
-                style="width:280px;height:395px;"
-                src="">
-              <div class="card-body">
-                <div class="title_num">2022年考研英语二</div><progress max="100" value="0"></progress>
-              </div>
-          </div>
-        </a></div>
-    </div>
-  </section>
-  <hr class=" m-5 mt-6 mb-6">
-</div></template>
+  <div class="catalogExamSection">
+    <a class="link-primary" href="/kaoyan/2022/02">
+      <div class="card">
+        <picture>
+          <source srcset="../../../assets/kaoyan/2022二.jpg" type="image/webp">
+          <source srcset="../../../assets/kaoyan/2022二.jpg"
+            type="image/jpg"><img class="card-img-top"
+            src="../../../assets/kaoyan/2022二.jpg"
+            style="width:280px;height:395px;" alt="2022年考研英语二">
+        </picture>
+        <div class="card-body">
+          <div class="title_num">2022年考研英语二</div><progress max="100" value="42">
+
+          </progress>
+        </div>
+      </div>
+    </a>
+  </div>
+</template>
 
 <script>
 export default {
@@ -51,4 +37,4 @@ export default {
   }
 }
 </script>
-<style scoped>/* @import url(); 引入css类 */</style>
+<style lang="scss" scoped>@import "./CatalogExamSection.scss";</style>
